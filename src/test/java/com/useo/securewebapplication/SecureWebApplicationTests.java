2507b712950d0e1212312337abf0e2bcbd338412
kimeswartz <kim.e.swartz@gmail.com>
2024-06-10 13:01:14 +0200

    Initial commit

---
+++ src/test/java/com/useo/securewebapplication/SecureWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.useo.securewebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
